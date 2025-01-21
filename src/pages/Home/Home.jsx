458abc3ffb230aadedc770190c4dc60a47b8c996
EssenceMaks <makski.wish@gmail.com>
2025-01-21 05:02:13 +0200

    Home Ready

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
+import '../../styles/colors.css';
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h1>Welcome to Our Camper Rental Service</h1>
+      <div className={styles.heroContent}>
+        <div className={styles.heroTitle}>Campers of your dreams</div>
+        <div className={styles.heroText}>You can find everything you want in our catalog</div>
+        <Link to="/catalog" className={styles.viewNowButton}>
+          View Now
+        </Link>
+      </div>
     </div>
   );
 };
